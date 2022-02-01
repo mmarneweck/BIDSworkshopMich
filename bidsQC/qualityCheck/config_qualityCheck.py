114c4e2469f46d1454c86fd6f7bda3eb42b3961c
mmarneweck <mmar@uoregon.edu>
2022-02-01 15:32:48 -0800

    change paths and specify directory contents in config file

--- bidsQC/qualityCheck/config_qualityCheck.py
+++ bidsQC/qualityCheck/config_qualityCheck.py
@@ -8,7 +8,7 @@ from timepoint import TimePoint
 # Set directories (Check these for your study)
 # These variables are used in the main script and need to be defined here. 
 # They need to exist prior to running the qualityCheck.py
-path_bidsdata = os.path.join(os.sep, 'projects', "sanlab", 'shared', "studyName", 'bids_data')  # Where your subjects' nifti directories are
+path_bidsdata = os.path.join(os.sep, 'projects', "motorskill", 'shared', "BIDSworkshopMich", 'bids_data')  # Where your subjects' nifti directories are
 logdir = os.path.join(os.getcwd(), 'logs_qualityCheck')  # Log files will go in the folder from which this script is run
 
 # Create a dictionary (the thing below) for each timepoint in your study.
@@ -16,12 +16,12 @@ logdir = os.path.join(os.getcwd(), 'logs_qualityCheck')  # Log files will go in 
 # e.g. Sequence('func', ...) means the directory is named func
 # The key:value pairs within the curly {} braces are taskName:numberOfRuns
 # e.g. a task named stopsignal that the subject completed twice would be entered as {'stopsignal':2}
-sequence1 = Sequence('func', {'stopsignal': 2, 'react':1})
-sequence3 = Sequence('anat', {'T1w':1})
-sequence4 = Sequence('fmap', {'dir-ap':1, 'dir-pa':1})  # Example of fieldmap with multiple phase encoded directions
-sequence5 = Sequence("fmap", {"magnitude1":1, "magnitude2":1, "phasediff":1 }) # Example of fieldmap with phase-difference map and multiple magnitude images
-timepoint1 = TimePoint('ses-wave1', [sequence1, sequence3, sequence4])   # Create an entry for each timepoint in your study
-timepoint2 = TimePoint('ses-wave2', [sequence1, sequence5])  # Add as many timepoints as needed and fill it with the correct sequences
+sequence1 = Sequence('func', {'bart': 1, 'gng1': 1,'gng2':1,'react1':1, 'react2':1, 'sst1':1, 'sst2':1})
+sequence2 = Sequence('anat', {'T1w':1})
+sequence3 = Sequence("fmap", {"magnitude1":2, "magnitude2":2, "phasediff":2 }) # Example of fieldmap with phase-difference map and multiple magnitude images
+sequence4 = Sequence('func', {'bart': 1, 'gng3': 1,'gng4':1,'react3':1, 'react4':1, 'sst3':1, 'sst4':1})
+timepoint1 = TimePoint('ses-wave1', [sequence1, sequence2, sequence3])   # Create an entry for each timepoint in your study
+timepoint2 = TimePoint('ses-wave2', [sequence4, sequence2, sequence3])  # Add as many timepoints as needed and fill it with the correct sequences
 expected_timepoints = [timepoint1, timepoint2]
 
 
@@ -35,7 +35,7 @@ gzipped = True
 # You need them to be recognized as multiple runs of the same task. 
 # List them below to have the 'run-#' field appended to indicate the order in which they were run.
 order_sequences = True
-tasks_to_order = 'stopsignal'    # comma seperated if multiple
+tasks_to_order = 'sst','gng','react'    # comma seperated if multiple
 
 
 ######################## DO NOT CHANGE ########################
